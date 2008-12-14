83f1cdd172d4d8bbbbb0750a74c4e984170d14d6
georgopoulos.georgios <georgopoulos.georgios@c0a477a4-a991-11dd-9c66-c9f8973bfffa>
2008-12-14 14:50:55 +0000

    GWT.create(InvokeBeanPropertyDescriptorGenerator.class) invokes the BeanPropertyDescriptorGenerator

---
+++ src/org/gwt/beansbinding/core/client/util/InvokeBeanPropertyDescriptorGenerator.java
@@ -0,0 +1,7 @@
+package org.gwt.beansbinding.core.client.util;
+
+public class InvokeBeanPropertyDescriptorGenerator {
+  public InvokeBeanPropertyDescriptorGenerator() {
+    // Nothimg to do here!
+  }
+}
