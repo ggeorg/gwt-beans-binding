8688fcfc9a4dac2d05ac5dd21b0c135afba0c3de
georgopoulos.georgios <georgopoulos.georgios@c0a477a4-a991-11dd-9c66-c9f8973bfffa>
2009-06-24 09:04:03 +0000

    (Update) Code format

--- trunk/src/org/gwt/beansbinding/core/client/util/GWTBeansBinding.java
+++ trunk/src/org/gwt/beansbinding/core/client/util/GWTBeansBinding.java
@@ -4,19 +4,19 @@ import com.google.gwt.core.client.GWT;
 
 public final class GWTBeansBinding extends Object {
 
-	public GWTBeansBinding() {
-		// Nothing to do here!
-	}
+  public GWTBeansBinding() {
+    // Nothing to do here!
+  }
 
-	public static void init() {
-		if (!GWT.isScript()) {
-			return;
-		}
-		try {
-			GWT.create(GWTBeansBinding.class);
-		} catch (Throwable t) {
-			GWT.log(t.getMessage(), t);
-		}
-	}
+  public static void init() {
+    if (!GWT.isScript()) {
+      return;
+    }
+    try {
+      GWT.create(GWTBeansBinding.class);
+    } catch (Throwable t) {
+      GWT.log(t.getMessage(), t);
+    }
+  }
 
 }
